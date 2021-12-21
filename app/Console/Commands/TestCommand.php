b4b8cf8c5855c4b69e50fd6141e85849850f99da
Dmitry Kuksin <mikopwnz@gmail.com>
2021-12-21 18:21:03 +0200

    second commit

---
+++ app/Console/Commands/TestCommand.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Events\TestEvent;
+use App\Jobs\TestJob;
+use Illuminate\Console\Command;
+
+class TestCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'test:test';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Test for laravel horizon';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        event(new TestEvent());
+        $this->info('OK');
+//        $job = new TestJob();
+//        dispatch($job);
+//        $this->info('OK');
+    }
+}
